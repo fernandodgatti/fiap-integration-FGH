3899a2dd329663c159118680e0ab0acb42f5cdf6
Abdala <hmabdala@gmail.com>
2021-03-07 21:09:27 -0300

    inserindo cors

--- api/app.js
+++ api/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 const port = 3000;
 let items = [];
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+
 app.get('/', (req, res) => {
     res.json(items);
-})
+});
 app.post('/', (req, res) => {
     items.push(req.body);
     res.send('Drone Adicionado com Sucesso!');
-})
+});
+app.put('/:id', (req, res))
 app.listen(port, () => {
     console.log('Controle de Drones');
-})
+});
